de79cc4cade7422f88e8a93340ec4d39e7f08147
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-15 20:26:22 -0400

    check that dom is loading

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("working");
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('http://localhost:3000/api/v1/looks')
+  .then(response => response.json())
+  .then(looks => {
+    console.log(looks);
+  })
+})
