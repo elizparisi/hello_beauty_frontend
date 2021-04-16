431b98652bf05e39fd41beb23a1eaf96510655b2
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-15 21:44:02 -0400

    create getProducts function

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const endPoint = 'http://localhost:3000/api/v1/products'
 
 document.addEventListener('DOMContentLoaded', () => {
+  getProducts()
+})
+
+function getProducts() {
   fetch(endPoint)
   .then(response => response.json())
   .then(products => {
     console.log(products);
   })
-})
+}
