101afee26f9bba38a7e2b3c4bcfc0d61c79ff5d3
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-22 17:58:30 -0400

    comment out code to check if each piece is working-fetch get request working

--- index.js
+++ index.js
@@ -13,27 +13,43 @@ function getProducts() {
   .then(response => response.json())
   .then(products => {
     // we don't want to console.log, we want to render the data
-    // console.log(products);
-    products.data.forEach (product => {
-      // our data is nested- refer to json info to see how it's nested
-      render(product)
-    })
-  .catch(error => console.log(error))
+    console.log(products);
+    // products.data.forEach (product => {
+    //   // our data is nested- refer to json info to see how it's nested
+    //   const productInfo = `
+    //     <div data-id=${product.id}>
+    //       <p>${product.attributes.category.name}</p>
+    //       <p>${product.attributes.original_product_image_url}</p>
+    //       <h4>${product.attributes.original_name}</h4>
+    //       <p>${product.attributes.original_price}</p>
+    //       <br>
+    //       <p>${product.attributes.dupe_product_image_url}</p>
+    //       <h4>${product.attributes.dupe_name}</h4>
+    //       <p>${product.attributes.dupe_price}</p>
+    //     </div>`
+      // render(product)
+  //   })
+  // .catch(error => console.log(error))
   })
 }
 
-function render(product) {
-  const productInfo = `
-    <div data-id=${product.id}>
-      <h4>${product.attributes.name}</h4>
-      <p>${product.attributes.category}</p>
-      <p>${product.attributes.look.name}</p>
-    </div>
-    <br>
-    <br>`;
-
-    document.querySelector('#product-container').innerHTML += productInfo
-}
+// function render(product) {
+//   const productInfo = `
+//     <div data-id=${product.id}>
+//       <p>${product.attributes.category.name}</p>
+//       <p>${product.attributes.original_product_image_url}</p>
+//       <h4>${product.attributes.original_name}</h4>
+//       <p>${product.attributes.original_price}</p>
+//       <br>
+//       <p>${product.attributes.dupe_product_image_url}</p>
+//       <h4>${product.attributes.dupe_name}</h4>
+//       <p>${product.attributes.dupe_price}</p>
+//     </div>
+//     <br>
+//     <br>;`
+//
+//     document.querySelector('#product-container').innerHTML += productInfo
+// }
 
 function createFormHandler(event) {
   event.preventDefault()
@@ -50,31 +66,30 @@ function createFormHandler(event) {
   postFetch(categoryInput, originalNameInput, originalPriceInput, originalImageInput, dupeNameInput, dupePriceInput, dupeImageInput)
 }
 
-function postFetch(look_id, category, name) {
-  // console.log(look_id, category, name);--works as expected
-  // make variable to hold body data, key + value are the same so only one is needed
-  const bodyData = {look_id, category, name}
-  fetch(endPoint, {
-    // POST request
-    method: "POST",
-    headers: { "Content-Type": "application/json"},
-    body: JSON.stringify(bodyData)
-  })
-  .then(response => response.json())
-  .then(product => {
-    // console.log(look);
-    const productData = product.data
-    // render JSON response
-    // const productInfo =  `
-    //   <div data-id=${product.id}>
-    //     <h4>${product.attributes.name}</h4>
-    //     <p>${product.attributes.category}</p>
-    //     <p>${product.attributes.look.name}</p>
-    //   </div>
-    //   <br>
-    //   <br>`;
-    //
-    //   document.querySelector('#product-container').innerHTML += productInfo
-    render(productData)
-  })
-}
+// function postFetch(category_id, original_product_name, original_product_price, original_product_image_url, dupe_product_name, dupe_product_price, dupe_product_image_url) {
+//   console.log(category_id, original_product_name, original_product_price, original_product_image_url, dupe_product_name, dupe_product_price, dupe_product_image_url);
+//   // make variable to hold body data, key + value are the same so only one is needed
+//   // const bodyData = {category_id, category, name}
+//   // fetch(endPoint, {
+//   //   // POST request
+//   //   method: "POST",
+//   //   headers: { "Content-Type": "application/json"},
+//   //   body: JSON.stringify(bodyData)
+//   // })
+//   // .then(response => response.json())
+//   // .then(product => {
+//   //   const productData = product.data
+//   //   // OLD INFO render JSON response
+//   //   // const productInfo =  `
+//   //   //   <div data-id=${product.id}>
+//   //   //     <h4>${product.attributes.name}</h4>
+//   //   //     <p>${product.attributes.category}</p>
+//   //   //     <p>${product.attributes.look.name}</p>
+//   //   //   </div>
+//   //   //   <br>
+//   //   //   <br>`;
+//   //   //
+//   //   //END OLD INFO  document.querySelector('#product-container').innerHTML += productInfo
+//   //   render(productData)
+//   // })
+// }
