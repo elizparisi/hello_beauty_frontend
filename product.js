9a7891331e5f4af707e8e4519e7f709871dc4614
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-24 02:38:38 -0400

    pass in productAttributes to Product constructor

--- product.js
+++ product.js
@@ -1,13 +1,13 @@
 class Product {
 
-  constructor(data) {
-    this.id = data.id
-    this.original_name = data.original_name
-    this.original_price = data.original_price
-    this.original_image_url = data.original_image_url
-    this.dupe_name = data.dupe_name
-    this.dupe_price = data.dupe_price
-    this.dupe_image_url = data.dupe_image_url
-    this.category = data.category
+  constructor(product, productAttributes) {
+    this.id = product.id
+    this.original_name = productAttributes.original_name
+    this.original_price = productAttributes.original_price
+    this.original_image_url = productAttributes.original_image_url
+    this.dupe_name = productAttributes.dupe_name
+    this.dupe_price = productAttributes.dupe_price
+    this.dupe_image_url = productAttributes.dupe_image_url
+    this.category = productAttributes.category
   }
 }
