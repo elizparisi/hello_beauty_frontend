d82549beeb5f927ba7d431805ec078473948149e
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-25 02:39:18 -0400

    set up cards to take info from product.js

--- product.js
+++ product.js
@@ -13,18 +13,48 @@ class Product {
   }
 
   render() {
+    // return `
+    //   <div data-id=${this.id}>
+    //     <p>${this.category.name}</p>
+    //     <img src=${this.original_image_url} height='375' width='350'/>
+    //     <h4>${this.original_name}</h4>
+    //     <p>${this.original_price}</p>
+    //     <br>
+    //     <br>
+    //     <img src=${this.dupe_image_url} height='375' width='350' />
+    //     <h4>${this.dupe_name}</h4>
+    //     <p>${this.dupe_price}</p>
+    //   </div>`;
     return `
-      <div data-id=${this.id}>
-        <p>${this.category.name}</p>
-        <img src=${this.original_image_url} height='200' width='250'/>
-        <h4>${this.original_name}</h4>
-        <p>${this.original_price}</p>
-        <br>
-        <br>
-        <img src=${this.dupe_image_url} height='200' width='250' />
-        <h4>${this.dupe_name}</h4>
-        <p>${this.dupe_price}</p>
-      </div>`;
+    <div class="row">
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card shadow-sm">
+          <img src=${this.original_image_url} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${this.original_name}</h5>
+          <p class="card-text">${this.original_price}</p>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+          <small class="text-muted">Category: ${this.category.name}</small>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="card">
+        <div class="card shadow-sm">
+          <img src=${this.dupe_image_url} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${this.dupe_name}</h5>
+          <p class="card-text">${this.dupe_price}</p>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+          <small class="text-muted">Category: ${this.category.name}</small>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    `
   }
 }
 
