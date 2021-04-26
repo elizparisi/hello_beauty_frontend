b708e2551179e4c07c005eda346c0dac60095337
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-04-25 23:33:03 -0400

    fixed indentation product.js

--- product.js
+++ product.js
@@ -1,31 +1,31 @@
 class Product {
 
-  constructor(product, productAttributes) {
-    this.id = product.id
-    this.original_name = productAttributes.original_name
-    this.original_price = productAttributes.original_price
-    this.original_image_url = productAttributes.original_image_url
-    this.dupe_name = productAttributes.dupe_name
-    this.dupe_price = productAttributes.dupe_price
-    this.dupe_image_url = productAttributes.dupe_image_url
-    this.category = productAttributes.category
-    Product.all.push(this)
-  }
-
-  render() {
-    // return `
-    //   <div data-id=${this.id}>
-    //     <p>${this.category.name}</p>
-    //     <img src=${this.original_image_url} height='375' width='350'/>
-    //     <h4>${this.original_name}</h4>
-    //     <p>${this.original_price}</p>
-    //     <br>
-    //     <br>
-    //     <img src=${this.dupe_image_url} height='375' width='350' />
-    //     <h4>${this.dupe_name}</h4>
-    //     <p>${this.dupe_price}</p>
-    //   </div>`;
-    return `
+	constructor(product, productAttributes) {
+		this.id = product.id
+		this.original_name = productAttributes.original_name
+		this.original_price = productAttributes.original_price
+		this.original_image_url = productAttributes.original_image_url
+		this.dupe_name = productAttributes.dupe_name
+		this.dupe_price = productAttributes.dupe_price
+		this.dupe_image_url = productAttributes.dupe_image_url
+		this.category = productAttributes.category
+		Product.all.push(this)
+	}
+  
+	render() {
+		// return `
+		//   <div data-id=${this.id}>
+		//     <p>${this.category.name}</p>
+		//     <img src=${this.original_image_url} height='375' width='350'/>
+		//     <h4>${this.original_name}</h4>
+		//     <p>${this.original_price}</p>
+		//     <br>
+		//     <br>
+		//     <img src=${this.dupe_image_url} height='375' width='350' />
+		//     <h4>${this.dupe_name}</h4>
+		//     <p>${this.dupe_price}</p>
+		//   </div>`;
+		return `
     <div class="row">
     <div class="col-sm-6">
       <div class="card" style="18rem";>
@@ -57,7 +57,7 @@ class Product {
   <br>
   <br>
     `
-  }
+	}
 }
 
 Product.all = [];
